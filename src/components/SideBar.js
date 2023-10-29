432b32dcb21ef29fcead14c7a5e298d5ba332540
Mahadev555 <mahadevmanerikar@gmail.com>
2023-10-29 21:20:55 +0530

    appslice,sidebar and store added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 const SideBar = () => {
     return (
-        <div className='p-5 shadow-lg'>
-            <h1>Subscriptions</h1>
+        <div className='p-5 shadow-lg w-48'>
+            
+            <ul>
+                <li>Home</li>
+                <li>Shorts</li>
+                <li>Videos</li>
+                <li>Live</li>
+            </ul>
+
+            <h1 className='font-bold py-5 pb-2'>Watch Later</h1>
+            <ul>
+                <li>Music</li>
+                <li>Sports</li>
+                <li>Gaming</li>
+                <li>Movies</li>
+            </ul>
+
+            <h1 className='font-bold py-5 pb-2'>Subscriptions</h1>
             <ul>
                 <li>Music</li>
                 <li>Sports</li>
@@ -11,7 +27,7 @@ const SideBar = () => {
                 <li>Movies</li>
             </ul>
         </div>
-    )
+    ) 
 }
 
 export default SideBar
