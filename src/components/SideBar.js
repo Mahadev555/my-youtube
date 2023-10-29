206b55be9fecb06576c51c081a5882f5ef83acd4
Mahadev555 <mahadevmanerikar@gmail.com>
2023-10-29 13:55:27 +0530

    new commit

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const SideBar = () => {
-  return (
-    <div>
-      <h1>SideBar</h1>
-    </div>
-  )
+    return (
+        <div className='p-5 shadow-lg'>
+            <h1>Subscriptions</h1>
+            <ul>
+                <li>Music</li>
+                <li>Sports</li>
+                <li>Gaming</li>
+                <li>Movies</li>
+            </ul>
+        </div>
+    )
 }
 
 export default SideBar
