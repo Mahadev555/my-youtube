1746f5b6b6a5868e8ed274397bac94b8370461fe
Mahadev555 <mahadevmanerikar@gmail.com>
2023-10-31 10:45:35 +0530

    youtube api key added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-const Youtube_videos_api = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=[YOUR_API_KEY]"
- 
+const Youtube_videos_api = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+api_key;
+
+const api_key = "AIzaSyD9c-DWdEOnqGYu_IPa-7a2AqjjD0Wnq2Y"
