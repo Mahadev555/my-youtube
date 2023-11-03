592ef70ee3ea0c2f63ef79ba4694fdbc7e91e0fe
Mahadev555 <mahadevmanerikar@gmail.com>
2023-11-04 00:39:23 +0530

    zx

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from './Button'
  
 
-const btnList = ["All","Live","Cricket","Music","5G","Drama","Gaming","News","Politics","Cooking","Battel grounds Mobile India"]
+const btnList = ["All","Live","Cricket","Music","5G","Drama","Gaming","Game","Politics","Cooking","Battel grounds Mobile India"]
 const ButtonList = () => {
   return (
     <div className="   flex mx-2">
