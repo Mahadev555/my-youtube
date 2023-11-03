49daef3aac3e1d78ed3fcc0baa28a1f7758b051b
Mahadev555 <mahadevmanerikar@gmail.com>
2023-11-04 00:52:21 +0530

    new

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import SideBar from './SideBar'
-import MainContainer from './MainContainer'
+import SideBar from './SideBar' 
 import { Outlet } from 'react-router-dom'
 
 const Body = () => {
