acbb4a972369f9157969d4d24411bf8323e00e03
Mahadev555 <mahadevmanerikar@gmail.com>
2023-11-04 00:19:38 +0530

    final updates of css of stage-1

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import Button from './Button'
+ 
 
-
-const btnList = ["All","Live","Cricket","Music","5G","Drama","Gaming","News","Politics","Cooking"]
+const btnList = ["All","Live","Cricket","Music","5G","Drama","Gaming","News","Politics","Cooking","Battel grounds Mobile India"]
 const ButtonList = () => {
   return (
     <div className="   flex mx-2">
     {btnList?.map((btnName, index) => {
       return <Button key={index} name={btnName} />;
     })}
+     
+     <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="mt-3   hover:rounded-full hover:bg-gray-100   cursor-pointer  ml-1 w-5 h-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
   </div>
   )
 }
 
 export default ButtonList
+
+
+ 
