418631ba2eab06e88bcb2fc9408c9ee7268826c4
Mahadev555 <mahadevmanerikar@gmail.com>
2023-11-05 01:49:34 +0530

    added uselocation ,usesearchParam to get videoid

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { closeMenu } from '../utils/appSlice'; 
-import { useParams } from 'react-router-dom';
+import { closeMenu } from '../utils/appSlice';  
+import { useLocation } from 'react-router-dom';
 
 const WatchPage = () => {
-// to get video id 
- const searchParams = useParams()
- const search = searchParams
- console.log(search)
+// to get video id from the link present in browser after clicking on thumbnail
+const location = useLocation();
+const searchParams = new URLSearchParams(location.search);
+const videoId = searchParams.get('v');
 
+console.log(videoId); 
 
 // to hide or dispatch sidebar after clicking on any video
   const dispatch = useDispatch();
@@ -23,7 +24,7 @@ const WatchPage = () => {
       className='rounded-xl mx-4 my-4'
       width="900" 
       height="505" 
-      src={"https://www.youtube.com/embed/"+search+"?si=Dt2rDpJCSrEgQDBf&autoplay=1" }
+      src={"https://www.youtube.com/embed/"+videoId+"?si=Dt2rDpJCSrEgQDBf&autoplay=1" }
       title="YouTube video player" 
        
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
