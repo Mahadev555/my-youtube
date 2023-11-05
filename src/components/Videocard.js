21fba66a2d48b74fc8c7d0413df6e30a74da19d2
Mahadev555 <mahadevmanerikar@gmail.com>
2023-11-05 23:01:12 +0530

    view count converted into M

--- src/components/Videocard.js
+++ src/components/Videocard.js
@@ -12,12 +12,14 @@ const Videocard = ({info}) => {
     const {channelTitle, title,thumbnails} = snippet;
 
   return (
-    <div className='px-4 py-3 w-full'>
+    <div className='px-4 py-2 w-full'>
       <img className='rounded-lg' alt='Thumbnail' src={thumbnails.medium.url}/>
       <ul className='p-2'>
         <li className='text-yt-spec-text-primary font-roboto font-sans text-m leading-2.2 font-medium overflow-hidden block max-h-4.4 line-clamp-2 box-border box-orient-vertical truncate'>{title}</li>
         <li>{channelTitle}</li>
-        <li>{statistics.viewCount} views </li>
+        <li>{
+           (statistics.viewCount/1000000)<1 ? Math.ceil((statistics.viewCount/1000) * 10) / 10+' K' : Math.ceil((statistics.viewCount/1000000) * 10) / 10+' M'
+        } views </li>
       </ul>
     </div>
   )
